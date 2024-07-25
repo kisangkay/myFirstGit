a26bf31c6e77c0f33b82e045a084fc7975f7dd53
Kip <kaykisang@gmail.com>
2024-07-25 13:40:21 +1000

    about to push index to 2nd branch

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
+//second branch
 function adds2nos(x,y){
     return x + y;
 
 }
 var sum = adds2nos(5,10);
 
-<<<<<<< HEAD
-console.log("Hello " + sum);
-=======
-console.log("Hello World" + sum); 
-
-//added comment
->>>>>>> main
+console.log("Hello World" + sum);
